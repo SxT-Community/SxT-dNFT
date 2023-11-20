c430b42083eb7afc87ddc42a610faca988c36553
ZaK <1734015+nopslip@users.noreply.github.com>
2023-11-20 10:10:29 -0700

    new URL for NFT metadata

--- contracts/FunctionsConsumer.sol
+++ contracts/FunctionsConsumer.sol
@@ -92,7 +92,7 @@ contract FunctionsConsumer is FunctionsClient, ConfirmedOwner, ERC721URIStorage 
   }
 
   function tokenURI(uint256) public view override(ERC721URIStorage) returns (string memory) {
-    string memory baseURL = "https://gateway.pinata.cloud/ipfs/QmYxCeAjwBiAHUztrFGt3e4ZZEV8txJdYSzVdk6YTWn84j/";
+    string memory baseURL = "https://blush-cognitive-shrimp-280.mypinata.cloud/ipfs/QmYxCeAjwBiAHUztrFGt3e4ZZEV8txJdYSzVdk6YTWn84j/";
     return  string(abi.encodePacked(baseURL, string(Strings.toString(SxTId))));
   }
 
